7f7e0cc55bd2e0a24a3825d42d61cbc61dbb5634
Boris <zhouboli@gmail.com>
2015-11-11 09:55:01 +0800

    redesigned

---
+++ APITool/APIMethodURLTableHeaderView.swift
@@ -0,0 +1,61 @@
+//
+//  APIMethodURLTableHeaderView.swift
+//  APITool
+//
+//  Created by Zhouboli on 15/11/11.
+//  Copyright © 2015年 Boris. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol APIMethodURLTableHeaderViewDelegate
+{
+    func APIMethodURL(tableHeaderView: APIMethodURLTableHeaderView, didTapMethod methodLabel: UILabel)
+}
+
+class APIMethodURLTableHeaderView: UIView
+{
+    var delegate: APIMethodURLTableHeaderViewDelegate?
+    var methodLabel: UILabel?
+    var urlTextField: UITextField?
+
+    override init(frame: CGRect)
+    {
+        super.init(frame: frame)
+        self.initSubviews()
+    }
+    
+    required init?(coder aDecoder: NSCoder)
+    {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func initSubviews()
+    {
+        let itemWidth = UIScreen.mainScreen().bounds.size.width - 20
+        let itemHeight: CGFloat = 50.0
+        
+        self.methodLabel = UILabel()
+        self.methodLabel?.frame = CGRectMake(10, 20, itemWidth, itemHeight)
+        self.methodLabel?.userInteractionEnabled = true
+        self.methodLabel?.addGestureRecognizer(UITapGestureRecognizer(target: self, action: "methodLabelTapped"))
+        self.methodLabel?.backgroundColor = UIColor.greenColor()
+        self.methodLabel?.textColor = UIColor.whiteColor()
+        self.methodLabel?.text = "GET"
+        self.methodLabel?.textAlignment = NSTextAlignment.Center
+        self.addSubview(self.methodLabel!)
+        
+        self.urlTextField = UITextField()
+        self.urlTextField?.frame = CGRectMake(10, 30 + itemHeight, itemWidth, itemHeight)
+        self.urlTextField?.backgroundColor = UIColor.whiteColor()
+        self.urlTextField?.placeholder = "Enter URL here"
+        self.addSubview(self.urlTextField!)
+    }
+    
+    func methodLabelTapped()
+    {
+        NSLog("methodLabelTapped")
+        self.delegate?.APIMethodURL(self, didTapMethod: self.methodLabel!)
+    }
+}
--- APITool/ViewController.swift
+++ APITool/ViewController.swift
@@ -9,11 +9,14 @@
 import UIKit
 import Foundation
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, APIMethodURLTableHeaderViewDelegate
 {
     var tableView: UITableView?
     var sendButton: UIButton?
+    var headerView: APIMethodURLTableHeaderView?
+    
     let sendButtonHeight: CGFloat = 60.0
+    let headerViewHeight: CGFloat = 200.0
     
     override func viewDidLoad()
     {
@@ -21,7 +24,13 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         self.view.backgroundColor = UIColor.whiteColor()
         self.title = "API Tool"
         
-        let tvRect = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height - sendButtonHeight)
+        let headerRect = CGRectMake(0, (self.navigationController?.navigationBar.frame.size.height)! + UIApplication.sharedApplication().statusBarFrame.height, self.view.frame.size.width, headerViewHeight)
+        self.headerView = APIMethodURLTableHeaderView(frame: headerRect)
+        self.headerView?.delegate = self
+        self.view.addSubview(self.headerView!)
+        
+        let tvTop: CGFloat = (self.navigationController?.navigationBar.frame.size.height)! + UIApplication.sharedApplication().statusBarFrame.height + headerViewHeight
+        let tvRect = CGRectMake(0, tvTop, self.view.frame.size.width, self.view.frame.size.height - sendButtonHeight - tvTop)
         self.tableView = UITableView(frame: tvRect, style: UITableViewStyle.Grouped)
         self.tableView?.backgroundColor = UIColor.lightGrayColor()
         self.tableView?.separatorStyle = .None
@@ -66,7 +75,28 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
     }
     
-    
+    // MARK: APIMethodURLTableHeaderViewDelegate
+    func APIMethodURL(tableHeaderView: APIMethodURLTableHeaderView, didTapMethod methodLabel: UILabel)
+    {
+        let alertcontroller = UIAlertController(title: "Method", message: "Select a method", preferredStyle: UIAlertControllerStyle.ActionSheet)
+        let getac = UIAlertAction(title: "GET", style: UIAlertActionStyle.Default) { (UIAlertAction) -> Void in
+            NSLog("GET")
+            self.headerView?.methodLabel?.text = "GET"
+        }
+        let postac = UIAlertAction(title: "POST", style: UIAlertActionStyle.Default) { (UIAlertAction) -> Void in
+            NSLog("POST")
+            self.headerView?.methodLabel?.text = "POST"
+        }
+        let cancelac = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel) { (UIAlertAction) -> Void in
+            //do something
+        }
+        alertcontroller.addAction(getac)
+        alertcontroller.addAction(postac)
+        alertcontroller.addAction(cancelac)
+        self.navigationController?.presentViewController(alertcontroller, animated: true, completion: { () -> Void in
+            //do something
+        })
+    }
     
     
     
